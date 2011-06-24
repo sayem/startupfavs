3ea2e63081e3f8a198fce61708e5fd5f427e255e
Sayem Islam <mail@sayemislam.com>
2011-06-24 18:09:06 -0400

    update

--- app/models/techpeeps.rb
+++ app/models/techpeeps.rb
@@ -6,22 +6,20 @@ class Techpeeps < ActiveRecord::Base
   serialize :last_tweet
 
   def self.daily
-    Techpeeps.limit(50).each do |techpeep|
+    Techpeeps.limit(3).each do |techpeep|
       user = Twitter.user(techpeep.twitter_id.to_i)
       if user.favourites_count != techpeep.count
+        current_fav = techpeep.last_tweet[1]
         favs = Twitter.favorites(techpeep.twitter_id.to_i)
-        current_fav = techpeep.last_tweet
-
-        tweets = favs.collect {|x| x.text }
-        index = tweets.index(current_fav)
-        favs.slice!(index..-1)
-        favs.collect! {|x| [x.user.screen_name, x.user.profile_image_url, x.text, Time.now.strftime("%m/%d/%Y")] }
-        techpeep.tweets = techpeep.tweets + favs
 
         latest_fav_id = favs.first.user.id
         latest_fav = favs.first.text
         techpeep.last_tweet = [latest_fav_id, latest_fav]
 
+        tweets = favs.collect {|x| x.text }
+        index = tweets.index(current_fav).to_i
+        favs.slice!(index..-1)
+        techpeep.tweets = favs.collect {|x| [x.user.screen_name, x.user.profile_image_url, x.text, Time.now.strftime("%m/%d/%Y")] }
         techpeep.count = user.favourites_count
         techpeep.save
       end
