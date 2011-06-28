196c0985f62c9253ab65b9363699c4a10339d5d7
Sayem Islam <mail@sayemislam.com>
2011-06-28 01:03:39 -0400

    updated design

--- app/models/techpeeps.rb
+++ app/models/techpeeps.rb
@@ -6,7 +6,7 @@ class Techpeeps < ActiveRecord::Base
   serialize :last_tweet
 
 
-  def self.daily
+  def self.daily1
     Techpeeps.limit(50).each do |techpeep|
       user = Twitter.user(techpeep.twitter_id.to_i)
       if user.favourites_count != techpeep.count
@@ -28,8 +28,46 @@ class Techpeeps < ActiveRecord::Base
     end
   end
 
-  def self.weekly
+  def self.daily2
+    Techpeeps.limit(50).offset(50).each do |techpeep|
+      user = Twitter.user(techpeep.twitter_id.to_i)
+      if user.favourites_count != techpeep.count
+        current_fav = techpeep.last_tweet[1]
+        favs = Twitter.favorites(techpeep.twitter_id.to_i)
+
+        latest_fav_id = favs.first.user.id
+        latest_fav = favs.first.text
+        techpeep.last_tweet = [latest_fav_id, latest_fav]
+
+        tweets = favs.collect {|x| x.text }
+        index = tweets.index(current_fav).to_i
+        favs.slice!(index..-1)
+        favs.collect! {|x| [x.user.screen_name, x.user.profile_image_url, x.text, Time.now.strftime("%m/%d/%Y")] }
+        techpeep.tweets = techpeep.tweets + favs
+        techpeep.count = user.favourites_count
+        techpeep.save
+      end
+    end
+  end
+
+  def self.weekly1
     Techpeeps.limit(50).each do |techpeep|
+      techpeep.tweets = []
+      user = Twitter.user(techpeep.twitter_id.to_i)
+      name = user.screen_name
+      pic = user.profile_image_url
+      if name != techpeep.username
+        techpeep.username = name
+      end
+      if pic != techpeep.picture
+        techpeep.picture = pic
+      end
+      techpeep.save
+    end
+  end
+
+  def self.weekly2
+    Techpeeps.limit(50).offset(50).each do |techpeep|
       techpeep.tweets = []
       user = Twitter.user(techpeep.twitter_id.to_i)
       name = user.screen_name
