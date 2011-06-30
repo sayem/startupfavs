3dfc4477ded590c771a18a355c2f2dd827f07578
Sayem Islam <mail@sayemislam.com>
2011-06-30 19:11:23 -0400

    update

--- twitter.rb
+++ twitter.rb
@@ -2,8 +2,11 @@
 require 'rubygems'
 require 'twitter'
 
+
 puts Twitter.rate_limit_status.remaining_hits.to_s + " Twitter API request(s) remaining this hour"
+
 
 #puts Twitter.user(16483617).favourites_count
 #puts Twitter.user(16483617)
-#puts Twitter.favorites('sayemislam').first.user.id
+#puts Twitter.favorites('jeffjarvis').first.text
+#puts Twitter.favorites('jeffjarvis').first.user.id
